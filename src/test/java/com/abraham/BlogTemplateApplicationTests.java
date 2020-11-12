49dced9b5e7bfa404533035532c67bcbd1ab3570
long <1486460308@qq.com>
2020-11-12 20:41:06 +0800

    create

---
+++ src/test/java/com/abraham/BlogTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
